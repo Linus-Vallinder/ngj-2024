ab3f376ad8e22e9f2abe6392cbe446b7bb3138f8
seehj <seeehj@gmail.com>
2024-04-20 11:04:18 +0200

    Dummy implementation of beat keeper

---
+++ Assets/BeatKeeper.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class BeatKeeper : MonoBehaviour
+{
+    [SerializeField]
+    protected AudioSource _MainSource;
+
+    protected double _SongDSPTime;
+    protected bool IsPlaying;
+
+    public int BarLength = 4;
+    public float BPM;
+    public float Offset;
+    public int CurrentCrotchetHit;
+    public int CurrentEigthHit;
+
+    public float SongPosition;
+
+    public float Crotchet
+    {
+        get
+        {
+            return 60.0f / BPM;
+        }
+    }
+
+    public int BarAmount
+    {
+        get
+        {
+            return (int)(_MainSource.clip.length / (BarLength * Crotchet));
+        }
+    }
+
+    public void Play()
+    {
+
+        _SongDSPTime = AudioSettings.dspTime;
+        _MainSource.Play();
+
+        CurrentCrotchetHit = 0;
+        CurrentEigthHit = 0;
+        IsPlaying = true;
+    }
+    
+    
+    //Shit thing, change for better input handling
+    void Update()
+    {
+        if (!IsPlaying)
+        {
+            return;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Yaay");
+        }
+        
+        SongPosition = (float)((AudioSettings.dspTime - _SongDSPTime) * _MainSource.pitch - Offset);
+        if (SongPosition > (CurrentCrotchetHit + 1) * Crotchet)
+        {
+            // Debug.Log("Yaay " + SongPosition + " huh " + (( 1 + CurrentHit) * Crotchet));
+            CurrentCrotchetHit += 1;
+
+        }
+        
+        if (SongPosition > (CurrentEigthHit + 1) * (Crotchet/2))
+        {
+            // Debug.Log("Yaay " + SongPosition + " huh " + (( 1 + CurrentHit) * Crotchet));
+            CurrentEigthHit += 1;
+
+        }
+    }
+}
---
+++ Assets/Timeline.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Timeline : MonoBehaviour
+{
+    [SerializeField] 
+    protected BeatKeeper _BeatKeeper;
+    
+    [SerializeField]
+    protected GameObject _ButtonPrefab;
+    
+    [SerializeField]
+    protected Transform _HitPosition;
+    
+    [SerializeField]
+    protected Transform _StartPosition;
+
+    protected List<GameObject> Buttons;
+    protected List<Thing> Pattern;
+    protected bool IsPlaying;
+    protected int HitCount;
+    protected float LengthBetweenCrotchetSeg;
+    protected float LengthBetweenEightsSeg;
+    protected float Length;
+    protected float PrevValue;
+    protected float Clock;
+    
+    private void Start()
+    {
+        Buttons = new List<GameObject>();
+        Pattern = new List<Thing>();
+
+        var dist = Vector3.Distance(_StartPosition.position, _HitPosition.position);
+        LengthBetweenCrotchetSeg = dist / 4.0f;
+        LengthBetweenEightsSeg = dist / 8.0f;
+        Length = Mathf.Abs(_StartPosition.position.y - _HitPosition.position.y);
+
+        for (int i = 0; i < 8; i++)
+        {
+            var btn = GameObject.Instantiate(_ButtonPrefab, this.transform);
+            btn.transform.position = _StartPosition.position + new Vector3(0, LengthBetweenCrotchetSeg, 0) * (i +1);
+            Buttons.Add(btn);
+        }
+
+        Thing thing = new Thing();
+        thing.Crotchet = true;
+        thing.BeatPosition = 0;
+        thing.Button = Buttons[0];
+        Pattern.Add(thing);
+        
+        thing = new Thing();
+        thing.BeatPosition = 4;
+        thing.Button = Buttons[1];
+        Pattern.Add(thing);
+        
+        thing = new Thing();
+        thing.BeatPosition = 5;
+        thing.Button = Buttons[2];
+        Pattern.Add(thing);
+        
+        thing = new Thing();
+        thing.Crotchet = true;
+        thing.BeatPosition = 2;
+        thing.Button = Buttons[3];
+        Pattern.Add(thing);
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _BeatKeeper.Play();
+            Play();
+        }
+        
+        if (!IsPlaying)
+        {
+            return;
+        }
+
+        Clock = PrevValue == _BeatKeeper.SongPosition
+            ? Clock + Time.unscaledDeltaTime
+            : _BeatKeeper.SongPosition;
+        
+        for (int i = 0; i < 4; i++)
+        {
+            var t = Pattern[i];
+            var fourthBeat = _BeatKeeper.CurrentCrotchetHit % 4;
+            var eightBeat = _BeatKeeper.CurrentEigthHit % 8;
+            var fourthTime = (Clock - ((_BeatKeeper.CurrentCrotchetHit) * _BeatKeeper.Crotchet) ) / _BeatKeeper.Crotchet;
+            var eightTime = (Clock - ((_BeatKeeper.CurrentEigthHit) * (_BeatKeeper.Crotchet / 2)) ) / (_BeatKeeper.Crotchet/2);
+            var btn = t.Button;
+            
+            if (t.Crotchet && fourthBeat == t.BeatPosition || btn.activeInHierarchy)
+            {
+                var pos = btn.transform.position;
+                pos.y = Mathf.Lerp(_StartPosition.position.y - LengthBetweenCrotchetSeg * t.InternalPos, _StartPosition.position.y - LengthBetweenCrotchetSeg * (t.InternalPos + 1), fourthTime);
+                btn.transform.position = pos;
+                btn.SetActive(true);
+
+                if (pos.y == _HitPosition.position.y)
+                {
+                    btn.SetActive(false);
+                }
+            }
+
+            if (!t.Crotchet && eightBeat == t.BeatPosition || btn.activeInHierarchy)
+            {
+                var pos = btn.transform.position;
+                pos.y = Mathf.Lerp(_StartPosition.position.y - LengthBetweenEightsSeg * eightBeat, _StartPosition.position.y - LengthBetweenEightsSeg * (eightBeat + 1), eightTime);
+                btn.transform.position = pos;
+                btn.SetActive(true);
+                
+                
+                if (pos.y == _HitPosition.position.y)
+                {
+                    btn.SetActive(false);
+                }
+            }
+            
+            // Debug.Log("time " + time + " song Pos: " + Clock + " thing " + (_BeatKeeper.CurrentHit + 1) * _BeatKeeper.Crotchet);
+        }
+
+        PrevValue = _BeatKeeper.SongPosition;
+    }
+
+    public void Play()
+    {
+        IsPlaying = true;
+    }
+}
+
+public struct Thing
+{
+    public GameObject Button;
+    public int BeatPosition;
+    public bool Crotchet;
+    public int InternalPos;
+}
